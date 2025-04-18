7b54cac8f3da2ed3ebe3cb3f31c4193b441ece32
28geekman <28geekman@gmail.com>
2025-04-18 17:10:51 +0100

    Edit 1 run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,17 @@ sudo apt full-upgrade -y
 
 
 echo "[+] Installing i3 window manager"
-sudo apt install -y xorg xinit i3
-sudo apt install -y rofi feh picom dunst lxappearance fonts-font-awesome thunar network-manager-gnome lxpolkit vim
+sudo apt install -y xorg xinit i3 firefox-esr
+sudo apt install -y rofi feh picom dunst lxappearance fonts-font-awesome thunar network-manager-gnome lxpolkit vim libnotify-bin
 
 echo "[+] Copying xinitrc"
 cp files/xinitrc ~/.xinitrc
+
+echo "[+] Copying background image"
+sudo mkdir /usr/share/backgrounds
+sudo cp files/arco-wallpaper.jpg /usr/share/backgrounds
+cp files/config ~/.config/i3/config
+
+
+
 
