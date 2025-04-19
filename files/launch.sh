7b46ea24527528ffec42a95b4bda9f0e75774912
28geekman <28geekman@gmail.com>
2025-04-19 05:53:02 +0100

    Added polybar config

---
+++ files/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Kill already running bars
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 0.5; done
+
+# Launch bar
+polybar hackbar &
